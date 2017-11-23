117be06f08b65e5969a4cc65f2d419eb1608afe8
rockylover <rakesh.narang@nagarro.com>
2017-11-23 16:52:26 +0530

    first commit

---
+++ functions_as_variables.js
@@ -0,0 +1,10 @@
+// assignment of a function to a variable
+var triple = function(x) {
+	return x * 3;
+}
+
+// passing around the function
+var waffle = triple;
+
+// this will return 90
+waffle(30);
